e5b24b2fafe31fa578861df69bb4be6f5cfacdae
Pamparau Victor <161034168+PilotViki@users.noreply.github.com>
2025-03-12 10:23:22 +0200

    Add files via upload

---
+++ MagazinSanitareElectrice/MagazinSanitareElectrice/EvidentaProduse/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagazinSanitareElectrice.EvidentaProduse
+{
+    class Program
+    {
+        static void Main()
+        {
+            AdministrareProduse_Memorie adminProduse = new AdministrareProduse_Memorie();
+            string optiune;
+
+            do
+            {
+                AfiseazaMeniu();
+                optiune = Console.ReadLine();
+
+                switch (optiune)
+                {
+                    case "1": adminProduse.AdaugaProdus(); break;
+                    case "2": adminProduse.AfiseazaProduse(); break;
+                    case "3": adminProduse.CautaProdus(); break;
+                    case "4": adminProduse.StergeProdus(); break;
+                    case "5": adminProduse.CreareListaProiect(); break;
+                    case "6": Console.WriteLine("Ieșire din aplicație."); return;
+                    default: Console.WriteLine("Opțiune invalidă. Alegeți din nou."); break;
+                }
+            } while (optiune != "6");
+        }
+
+        static void AfiseazaMeniu()
+        {
+            Console.WriteLine("\n===== Meniu Magazin =====");
+            Console.WriteLine("1. Adăugare produs");
+            Console.WriteLine("2. Afișare produse");
+            Console.WriteLine("3. Căutare produs după ID");
+            Console.WriteLine("4. Ștergere produs");
+            Console.WriteLine("5. Creare listă pentru proiect");
+            Console.WriteLine("6. Ieșire");
+            Console.Write("Alege o opțiune: ");
+        }
+    }
+}
---
+++ MagazinSanitareElectrice/MagazinSanitareElectrice/NivelStocareDate/AdministrareProduse_Memorie.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagazinSanitareElectrice.NivelStocareDate
+{
+    {
+    public class AdministrareProduse_Memorie
+    {
+        private const int NR_MAX_PRODUSE = 100;
+        private Produs[] produse;
+        private int nrProduse;
+
+        public AdministrareProduse_Memorie()
+        {
+            produse = new Produs[NR_MAX_PRODUSE];
+            nrProduse = 0;
+        }
+
+        public void AdaugaProdus(Produs produs)
+        {
+            if (nrProduse < NR_MAX_PRODUSE)
+            {
+                produse[nrProduse] = produs;
+                nrProduse++;
+            }
+            else
+            {
+                Console.WriteLine("Nu se mai pot adăuga produse, stoc maxim atins!");
+            }
+        }
+
+        public Produs[] GetProduse(out int nrProduse)
+        {
+            nrProduse = this.nrProduse;
+            return produse;
+        }
+
+        public Produs? CautaProdus(int idProdus)
+        {
+            foreach (var produs in produse)
+            {
+                if (produs != null && produs.IdProdus == idProdus)
+                {
+                    return produs;
+                }
+            }
+            return null;
+        }
+
+        public void StergeProdus(int idProdus)
+        {
+            for (int i = 0; i < nrProduse; i++)
+            {
+                if (produse[i].IdProdus == idProdus)
+                {
+                    for (int j = i; j < nrProduse - 1; j++)
+                    {
+                        produse[j] = produse[j + 1];
+                    }
+                    produse[nrProduse - 1] = null;
+                    nrProduse--;
+                    Console.WriteLine("Produs șters cu succes!");
+                    return;
+                }
+            }
+            Console.WriteLine("Produsul nu a fost găsit!");
+        }
+    }
+}
+
+
